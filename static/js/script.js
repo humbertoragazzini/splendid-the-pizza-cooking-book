7c47f9327bf7d686b5b72d9b01a7a43615639a50
humbertoragazzini <beto_vicio@hotmail.com>
2021-11-18 20:05:59 +0000

    mtesting own toast to check rendering problems in s9 plus

--- static/js/script.js
+++ static/js/script.js
@@ -2,23 +2,8 @@ var toast = $('.toast').toast(
     animation = true,
     autohide = true,
     delay=500,
-);
+).show();
 
-toast.show();
 
-var toast = $('.toast').toast(
-    animation = true,
-    autohide = true,
-    delay=500,
-);
-
-toast.show();
-
-function hideTheToast(thetoast){
-    thetoast.hide();
-}
-
-toast.show();
-setTimeout(hideTheToast, 800000, toast);
 
 
