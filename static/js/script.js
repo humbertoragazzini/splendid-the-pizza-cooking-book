dd2e03f03c7d434c5150430dd7cfc5fbcdc88a47
humbertoragazzini <beto_vicio@hotmail.com>
2021-11-19 00:00:12 +0000

    fixed position, fixed animation, fixed problem with accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,16 @@
 
-
+var locker = document.getElementsByClassName("add-space");
 $('.toast').toast().show();
 
-function closeToast(){
+function closeToast(id){
     $('.toast').toast().hide();
+    space = document.getElementById(id);
+    space.classList.toggle('hidden-space');
 }
 
-
+function resetSpaces(){
+    for(i=0;i<locker.length();i++)
+        {
+            locker[i].classList.toggle('hidden-space');
+        }
+}
